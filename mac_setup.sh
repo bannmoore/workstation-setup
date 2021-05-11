9acd8f869324f4da042d4916f0075fb7f5b8d329
bannmoore <moore.brittanyann@gmail.com>
2021-05-11 13:11:52 -0400

    Add default branch git configuration.

--- mac_setup.sh
+++ mac_setup.sh
@@ -135,6 +135,7 @@ echo "## configure git"
 git config --global user.name $GITHUB_USERNAME
 git config --global user.email $GITHUB_EMAIL
 git config --global core.editor 'code --wait'
+git config --global init.defaultBranch main
 git config --global merge.ff only
 git config --global pull.rebase true
 git config --global fetch.prune true
